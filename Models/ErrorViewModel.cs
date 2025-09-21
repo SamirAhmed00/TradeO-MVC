01e3b9dc3c7a951ca4a4de748f6f4ce7d99f2f31
Samir <basm.fathy159@gmail.com>
2025-09-21 19:05:57 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TradeO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
